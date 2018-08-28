87e15bc5cbafcc64e2122c7c73ee8e3a9aea06d9
Cristian Fat <dcristian353@gmail.com>
2018-08-28 20:31:06 +0100

    last changes

--- README.md
+++ README.md
@@ -2,9 +2,7 @@
 ![takapp](https://taskappexer.herokuapp.com/favicons/taskapp.png)
 Task management application created using Laravel & Vue.js
 
- Simple CRUD using Vue components.
-
-[Demo](https://taskappexer.herokuapp.com)
+Simple CRUD using Vue components.
 
 ## Created by [@cristianexer](https://mugscript.wordpress.com)
 
--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'db4free.net'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'taskappdatabase'),
-            'username' => env('DB_USERNAME', 'taskappexer'),
-            'password' => env('DB_PASSWORD', 'taskappDatabasePassword35'),
+            'database' => env('DB_DATABASE', 'taskapp'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', 'database'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
